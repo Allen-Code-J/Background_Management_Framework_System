c10b11a476ed1212399b2ffc600d1e15b87e9e7a
ChongyaJin <352577661@qq.com>
2022-11-01 22:19:13 -0700

    Springboot

---
+++ Scan QR Code/temp/springboot/src/main/java/com/example/springboot/service/IUserService.java
@@ -0,0 +1,18 @@
+package com.example.springboot.service;
+
+import com.example.springboot.controller.dto.UserDTO;
+import com.example.springboot.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Jin
+ * @since 2022-10-30
+ */
+public interface IUserService extends IService<User> {
+
+    UserDTO login(UserDTO userDTO);
+}
