11abc460b30d32f0505c49c3d0c045253ca42ade
ChongyaJin <352577661@qq.com>
2022-11-06 19:59:26 -0800

    Springboot

--- Scan QR Code/springboot/vue/src/views/Home.vue
+++ Scan QR Code/springboot/vue/src/views/Home.vue
@@ -1,14 +1,48 @@
 <template>
   <div>
-    <h1>
-      这是主页
-    </h1>
+
+    <div id = "main" style = "width: 500px; height: 400px"></div>
   </div>
 </template>
 
 <script>
+import * as echarts from 'echarts'
+
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+
+    }
+  },
+  mounted() { //页面元素渲染之后再触发
+
+
+    var chartDom = document.getElementById('main');
+    var myChart = echarts.init(chartDom);
+    var option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'bar'
+        },
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }
+      ]
+    };
+
+    myChart.setOption(option);
+
+  }
 }
 </script>
 
