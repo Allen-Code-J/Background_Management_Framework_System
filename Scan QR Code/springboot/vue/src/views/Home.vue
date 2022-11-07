d217881ef76b1fea74f8a71364ebda4eca74d867
ChongyaJin <352577661@qq.com>
2022-11-06 22:09:18 -0800

    Springboot

--- Scan QR Code/springboot/vue/src/views/Home.vue
+++ Scan QR Code/springboot/vue/src/views/Home.vue
@@ -1,7 +1,62 @@
 <template>
   <div>
+    <el-row :gutter="10" style="margin-bottom: 40px">
+      <el-col :span="6">
+        <el-card>
+          <div >
+            用户总数
+          </div>
+          <div style="padding: 10px 0; text-align: center; font-weight: bold">
+            100
+          </div>
+        </el-card>
+      </el-col>
 
-    <div id = "main" style = "width: 500px; height: 400px"></div>
+      <el-col :span="6">
+        <el-card>
+          <div>
+            <div >
+              销售总量
+            </div>
+            <div style="padding: 10px 0; text-align: center; font-weight: bold">
+              $ 1000000
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+
+      <el-col :span="6">
+        <el-card>
+          <div >
+            收益总额
+          </div>
+          <div style="padding: 10px 0; text-align: center; font-weight: bold">
+            $3000
+          </div>
+        </el-card>
+      </el-col>
+
+      <el-col :span="6">
+        <el-card>
+          <div >
+            门店总数
+          </div>
+          <div style="padding: 10px 0; text-align: center; font-weight: bold">
+            20
+          </div>
+        </el-card>
+      </el-col>
+
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <div id="main" style="width: 500px; height: 400px"></div>
+      </el-col>
+      <el-col :span="12">
+        <div id="pie" style="width: 500px; height: 400px"></div>
+      </el-col>
+    </el-row>
+
   </div>
 </template>
 
@@ -11,37 +66,98 @@ import * as echarts from 'echarts'
 export default {
   name: "Home",
   data() {
-    return {
-
-    }
+    return {}
   },
-  mounted() { //页面元素渲染之后再触发
-
-
-    var chartDom = document.getElementById('main');
-    var myChart = echarts.init(chartDom);
-    var option = {
+  mounted() {//页面元素渲染之后再触发
+    const option = {
+      title: {
+        text: '各季度会员数量统计',
+        subtext: '趋势图',
+        left: 'center'
+      },
       xAxis: {
         type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        data: ["第一季度", "第二季度", "第三季度", "第四季度"]
       },
       yAxis: {
         type: 'value'
       },
       series: [
         {
-          data: [150, 230, 224, 218, 135, 147, 260],
-          type: 'bar'
+          data: [],
+          type: 'line'
         },
         {
-          data: [150, 230, 224, 218, 135, 147, 260],
-          type: 'line'
+          data: [],
+          type: 'bar'
         }
       ]
     };
+    var pieOption = {
+      title: {
+        text: '各季度会员数量统计',
+        subtext: '比例图',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left'
+      },
+      series: [
+        {
+          type: 'pie',
+          radius: '60%',
+          label: {            //饼图图形上的文本标签
+            normal: {
+              show: true,
+              position: 'inner', //标签的位置
+              textStyle: {
+                fontWeight: 300,
+                fontSize: 14,    //文字的字体大小
+                color: "#fff"
+              },
+              formatter: '{d}%'
+            }
+          },
+          data: [],  // 填空
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    };
+    var chartDom = document.getElementById('main');
+    var myChart = echarts.init(chartDom);
 
-    myChart.setOption(option);
+    var pieDom = document.getElementById('pie');
+    var pieChart = echarts.init(pieDom);
 
+    this.request.get("/echarts/members").then(res => {
+      // 填空
+      // option.xAxis.data = res.data.x
+      option.series[0].data = res.data
+      option.series[1].data = res.data
+      // 数据准备完毕之后再set
+      myChart.setOption(option);
+
+    })
+    this.request.get("/echarts/members").then(res => {
+
+      pieOption.series[0].data = [
+        {name: "第一季度", value: res.data[0]},
+        {name: "第二季度", value: res.data[1]},
+        {name: "第三季度", value: res.data[2]},
+        {name: "第四季度", value: res.data[3]},
+      ]
+      pieChart.setOption(pieOption)
+    })
   }
 }
 </script>
