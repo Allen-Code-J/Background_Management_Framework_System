398e930f175e998b041550e7365f3044029cd088
ChongyaJin <352577661@qq.com>
2022-11-09 20:50:26 -0800

    Quote

---
+++ PromiseExample/PromiseExample/CompanyQuote.swift
@@ -0,0 +1,14 @@
+//
+//  CompanyQuote.swift
+//  PromiseExample
+//
+//  Created by 醒着像睡着 on 11/9/22.
+//
+
+import Foundation
+
+class CompanyQuote {
+    var symbol : String = ""
+    var name : String = ""
+    var price : Float = 0.0
+}
