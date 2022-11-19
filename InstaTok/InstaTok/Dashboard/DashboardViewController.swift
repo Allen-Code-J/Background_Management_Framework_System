9ec8dc860e55fbd2080277f0f18ae19b9bf46d18
ChongyaJin <352577661@qq.com>
2022-11-18 20:33:32 -0800

    A

--- InstaTok/InstaTok/Dashboard/DashboardViewController.swift
+++ InstaTok/InstaTok/Dashboard/DashboardViewController.swift
@@ -7,21 +7,60 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
 class DashboardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let arr = ["Ashish", "Mark", "Tom", "Bill"]
     
+    var posts: [PostsClass] = [PostsClass]()
+    
     
 
     @IBOutlet weak var tblView: UITableView!
+    
+    var db: Firestore?
+    
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        db = Firestore.firestore()
+        getPosts()
 
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func getPosts() {
+        guard let uid = Auth.auth().currentUser?.uid else {return}
+        guard let db = db else {return}
+        db.collection("Posts").whereField("UID", isEqualTo: uid)
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    self.posts = [PostsClass]()
+                    for document in querySnapshot!.documents {
+                        //print("\(document.documentID) => \(document.data())")
+                        
+                        guard let values = document.data() as? [String: Any] else {return}
+                        
+                        guard let caption = values["Caption"] as? String else {return}
+                        guard let imageURL = values["ImageURL"] as? String else {return}
+                        guard let location = values["Location"] as? String else {return}
+                        
+                        let post = PostsClass()
+                        post.caption = caption
+                        post.dateTime = ""
+                        post.location = location
+                        post.imageURL = imageURL
+                        self.posts.append(post)
+                    }
+                    self.tblView.reloadData()
+                }
+        }
     }
     
 
@@ -42,19 +81,35 @@ class DashboardViewController: UIViewController, UITableViewDelegate, UITableVie
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arr.count
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = Bundle.main.loadNibNamed("imagePostTableViewCell", owner: self)?.first as! imagePostTableViewCell
-                
+
         cell.imgView.image = UIImage(named: "LA")
-        cell.caption.text = "Caption"
-        cell.location.text = "Location"
-        cell.dateTime.text = "DateTime"
+        cell.caption.text = posts[indexPath.row].caption
+        cell.location.text = posts[indexPath.row].location
+        cell.dateTime.text = "Date Time"
 
         return cell
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = arr[indexPath.row]
+//        return cell
     }
+    
+    /**
+     db.collection("cities").whereField("capital", isEqualTo: true)
+         .getDocuments() { (querySnapshot, err) in
+             if let err = err {
+                 print("Error getting documents: \(err)")
+             } else {
+                 for document in querySnapshot!.documents {
+                     print("\(document.documentID) => \(document.data())")
+                 }
+             }
+     }
+     */
     
 }
     
---
+++ InstaTok/InstaTok/Models/PostsClass.swift
@@ -0,0 +1,15 @@
+//
+//  PostsClass.swift
+//  InstaTok
+//
+//  Created by 醒着像睡着 on 11/18/22.
+//
+
+import Foundation
+
+class PostsClass {
+    var caption: String = ""
+    var location: String = ""
+    var dateTime: String = ""
+    var imageURL: String = ""
+}
